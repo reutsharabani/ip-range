04fb1b6f9a7592143488dc6fbedf21a1cb7118f2
Reut Sharabani <reut@cyberint.com>
2018-08-15 12:42:57 +0000

    wrapped test in test functions for pytest to run them

--- iprange/tests/test_ip_range.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from ipaddress import IPv4Address
-from iprange.iprange import IPv4Range
-
-assert list(IPv4Range(0, 5)) == list(map(IPv4Address, range(5)))
-assert IPv4Range(0, 5)[1] == IPv4Address(1)
-assert IPv4Address(0) in IPv4Range(0, 5)
-assert IPv4Address(5) not in IPv4Range(0, 5)
-assert IPv4Range(0, 5) | IPv4Range(5,10) == IPv4Range(0, 10)
