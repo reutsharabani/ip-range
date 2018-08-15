4ff71dc2aa35dc9d8962518c5ce4b3bd0adcb127
Reut Sharabani <reut@cyberint.com>
2018-08-15 14:21:21 +0000

    updated setup.py, updated README

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Demo implementation of an IPv4Range based on python's standard `range` function.
+Demo implementation of an IPv4Range based on python's standard `range` function the `ipaddress` module.
--- setup.py
+++ setup.py
@@ -6,10 +6,21 @@ def read_file(name):
         return fp.read()
 
 setuptools.setup(
-    name='iprange',
-    version='0.1',
+    name='ip-range',
+    version='0.0.1',
     long_description=read_file('README.md'),
     packages=['iprange'],
     tests_require=['pytest'],
-    setup_requires=['pytest-runner']
+    setup_requires=['pytest-runner'],
+    author="Reut Sharabani",
+    author_email="reut.sharabani@gmail.com",
+    description="library to handle ip ranges (using `ipaddress`)",
+    url="https://github.com/reutsharabani/ipv4range",
+    classifiers=(
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries",
+    ),
 )
