7b3411f8a22238d7b84ba962802a5b3c3c55ccdd
Reut Sharabani <reut@cyberint.com>
2018-08-19 14:17:09 +0000

    parametrized all tests (and added some)

--- tests/test_ip_range.py
+++ tests/test_ip_range.py
@@ -1,30 +1,57 @@
 #!/usr/bin/env python
+import pytest
+
 from ipaddress import IPv4Address
 from iprange.iprange import IPv4Range
 
 
 # parametrize all tests to run much more data
 
-
-def test_list():
-    assert list(IPv4Range(0, 5)) == list(map(IPv4Address, range(5)))
-
-
-def test_getitem():
-    assert IPv4Range(0, 5)[1] == IPv4Address(1)
-
-
-def test_contains():
-    assert IPv4Address(0) in IPv4Range(0, 5)
+@pytest.mark.parametrize('ip_range, expected_output',
+        [
+            (IPv4Range(0, 3), [IPv4Address(0), IPv4Address(1), IPv4Address(2)]),
+            (IPv4Range(0, 1), [IPv4Address(0)])
+        ])
+def test_list(ip_range, expected_output):
+    assert list(ip_range) == expected_output
 
 
-def test_not_contains():
-    assert IPv4Address(5) not in IPv4Range(0, 5)
+@pytest.mark.parametrize('ip_range,index,expected_output',
+        [
+            (IPv4Range(0, 1), 0, IPv4Address(0)),
+            (IPv4Range(0, 1), -1, IPv4Address(0)),
+            (IPv4Range(0, 2), -1, IPv4Address(1)),
+            (IPv4Range(0, 2), -2, IPv4Address(0)),
+        ])
+def test_getitem(ip_range, index, expected_output):
+    assert ip_range[index] == expected_output
 
 
-def test_or_eq():
-    assert IPv4Range(0, 5) | IPv4Range(5,10) == IPv4Range(0, 10)
+@pytest.mark.parametrize('ip_address, ip_range, expected_output',
+        [
+            (IPv4Address(0), IPv4Range(0, 1), True),
+            (IPv4Address(1), IPv4Range(0, 1), False),
+            (IPv4Address(0), IPv4Range(1, 2), False),
+            (IPv4Address(2), IPv4Range(1, 3), True),
+        ])
+def test_contains(ip_address, ip_range, expected_output):
+    assert (ip_address in ip_range) == expected_output
 
 
-def test_len():
-    assert len(IPv4Range(0, 5)) == 5
+
+@pytest.mark.parametrize('ip_range1, ip_range2, expected_output',
+        [
+            (IPv4Range(1,2), IPv4Range(1, 3), IPv4Range(1,3)),
+            (IPv4Range(1,2), IPv4Range(2, 3), IPv4Range(1,3)),
+        ])
+def test_or_eq(ip_range1, ip_range2, expected_output):
+    assert ip_range1 | ip_range2 == expected_output
+
+
+@pytest.mark.parametrize('ip_range, expected_output',
+        [
+            (IPv4Range(1,2), 1),
+            (IPv4Range(0, 5), 5),
+        ])
+def test_len(ip_range, expected_output):
+    assert len(ip_range) == expected_output
